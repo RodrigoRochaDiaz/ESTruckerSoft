da7abf9ddc7df7d747de8b4daa403135d875be5e
RodrigoRochaDiaz <rodrigo.rocha.diaz@gmail.com>
2017-10-04 21:50:02 -0500

    EsTruckerSoft

--- app/src/main/java/com/example/htpc/estruckersoft/Activities/MainActivity.java
+++ app/src/main/java/com/example/htpc/estruckersoft/Activities/MainActivity.java
@@ -3,17 +3,75 @@ package com.example.htpc.estruckersoft.Activities;
 import android.os.Bundle;
 import com.example.htpc.estruckersoft.Base.ActivityBase;
 import com.example.htpc.estruckersoft.R;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class MainActivity extends ActivityBase {
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            mapView = (MapView) findViewById(R.id.mapView);
+            mapView.onCreate(savedInstanceState);
+            mapView.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(MapboxMap mapboxMap) {
+
+                }
+            });
+        }
     }
 
     @Override
     public void bindUserInterface() {
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
--- app/src/main/java/com/example/htpc/estruckersoft/Utils/Constants.java
+++ app/src/main/java/com/example/htpc/estruckersoft/Utils/Constants.java
@@ -2,6 +2,6 @@ package com.example.htpc.estruckersoft.Utils;
 
 public class Constants {
     public static final String EMPTY_STRING = "";
-    public static final boolean DONT_USE_HISTORY = true;
+    public static final boolean DONT_USE_HISTORY = false;
     public static final boolean USE_HISTORY = true;
 }
